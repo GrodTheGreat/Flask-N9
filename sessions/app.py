85346efc97067a76b551eb777e3456c7d6d8dfb6
Gerard Causse <grod.causse@gmail.com>
2025-01-18 18:59:13 -0500

    Clean up formatting

--- sessions/app.py
+++ sessions/app.py
@@ -1,11 +1,22 @@
-from flask import Flask, render_template, session, make_response, request, flash
+from flask import (
+    flash,
+    Flask,
+    make_response,
+    render_template,
+    request,
+    session
+)
 
 app = Flask(import_name=__name__, template_folder='templates')
-app.secret_key = 'KEY GOES HERE'  # In production this should be something serious
+# In production this should be something serious
+app.secret_key = 'KEY GOES HERE'
 
 @app.route(rule='/')
 def index():
-    return render_template(template_name_or_list='index.html', message='Index')
+    return render_template(
+        template_name_or_list='index.html',
+        message='Index'
+    )
 
 
 @app.route(rule='/set_data')
@@ -13,7 +24,10 @@ def set_data():
     session['name'] = 'Mike'
     session['other'] = 'Hello, World!'
 
-    return render_template(template_name_or_list='index.html', message='Session data set.')
+    return render_template(
+        template_name_or_list='index.html',
+        message='Session data set.'
+    )
 
 
 @app.route(rule='/get_data')
@@ -22,9 +36,15 @@ def get_data():
         name = session['name']
         other = session['other']
 
-        return render_template(template_name_or_list='index.html', message=f'Name: {name}, Other: {other}')
+        return render_template(
+            template_name_or_list='index.html',
+            message=f'Name: {name}, Other: {other}'
+        )
     else:
-        return render_template(template_name_or_list='index.html', message='No session found')
+        return render_template(
+            template_name_or_list='index.html',
+            message='No session found'
+        )
 
 
 @app.route('/clear_session')
@@ -32,12 +52,19 @@ def clear_session():
     session.clear()
     # session.pop('name')  # If you only want to clear a single item
 
-    return render_template(template_name_or_list='index.html', message='Session cleared')
+    return render_template(
+        template_name_or_list='index.html',
+        message='Session cleared'
+    )
 
 
 @app.route('/set_cooke')
 def set_cookie():
-    response = make_response(render_template(template_name_or_list='index.html', message='Cookie set.'))
+    response = make_response(
+        render_template(
+            template_name_or_list='index.html',
+            message='Cookie set.'
+        ))
     response.set_cookie(key='cookie_name', value='cookie_value')
 
     return response
@@ -47,12 +74,19 @@ def set_cookie():
 def get_cookie():
     cookie_value = request.cookies['cookie_name']
 
-    return render_template(template_name_or_list='index.html', message=f'Cookie Value: {cookie_value}')
+    return render_template(
+        template_name_or_list='index.html',
+        message=f'Cookie Value: {cookie_value}'
+    )
 
 
 @app.route('/remove_cooke')
 def remove_cookie():
-    response = make_response(render_template(template_name_or_list='index.html', message='Cookie removed.'))
+    response = make_response(
+        render_template(
+            template_name_or_list='index.html',
+            message='Cookie removed.'
+        ))
     response.set_cookie(key='cookie_name', expires=0)
 
     return response
@@ -67,10 +101,18 @@ def login():
         password = request.form.get('password')
         if username == 'neuralninja' and password == 'password':
             flash('Successful login!')
-            return render_template(template_name_or_list='index.html', message='')
+            
+            return render_template(
+                template_name_or_list='index.html',
+                message=''
+            )
         else:
             flash('Login failed!')
-            return render_template(template_name_or_list='index.html', message='')
+
+            return render_template(
+                template_name_or_list='index.html',
+                message=''
+            )
 
 
 if __name__ == '__main__':
